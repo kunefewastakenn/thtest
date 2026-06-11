54f0e8c6df4812cab5a97e8f04ca4cdb7e2dbac8
PixelOyuncu <pixeloyuncuplayz@gmail.com>
2026-06-11 17:37:23 +0300

    bigo fixo

---
+++ include/supertext.h
@@ -0,0 +1,16 @@
+//
+// Created by pixeloyuncu on 6/11/2026.
+//
+
+#ifndef THTEST_SUPERTEXT_H
+#define THTEST_SUPERTEXT_H
+#include <string>
+
+typedef struct supertext {
+    std::string text;
+    float size;
+    float x; // 0-1
+    float y; // 0-1
+};
+
+#endif //THTEST_SUPERTEXT_H
