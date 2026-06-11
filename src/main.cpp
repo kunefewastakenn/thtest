cd2d3d802cc2852f562e72eff86cdcc501e83542
PixelOyuncu <pixeloyuncuplayz@gmail.com>
2026-06-11 18:39:49 +0300

    simple tower defense 2

--- src/main.cpp
+++ src/main.cpp
@@ -29,31 +29,28 @@ void loadingScreen() {
 }
 
 int main() {
-    InitWindow(800, 450, "Touhou: Through the Bamboo Forest");
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(800, 450, "Touhou: Through the Bamboo Forest");
 
     std::thread loadingThread(loadingScreen); // thread for loading text
-
-    while (!loaded) {
-        BeginDrawing();
-        ClearBackground(BLACK);
-
-        int fontSize = 24;
-        int textWidth = MeasureText(initText.c_str(), fontSize);
-        DrawText(initText.c_str(), (GetScreenWidth() / 2) - (textWidth / 2), 225, fontSize, WHITE);
-
-        DrawRectanglePro({GetScreenWidth()/2.0f, GetScreenHeight()/2.0f+100, 40, 40}, {20, 20},
-            (clock()/4) % 360, WHITE);
-
-        EndDrawing();
-    }
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        int textWidth = MeasureText("VICTORY :D", 35);
-        DrawText("VICTORY :D", (GetScreenWidth() / 2) - (textWidth / 2), 225, 35, YELLOW);
+        DrawText(TextFormat("%i FPS", GetFPS()), 10, 10, 16, LIGHTGRAY);
+
+        if (!loaded) {
+            int fontSize = 24;
+            int textWidth = MeasureText(initText.c_str(), fontSize);
+            DrawText(initText.c_str(), (GetScreenWidth() / 2) - (textWidth / 2), 225, fontSize, WHITE);
+
+            DrawRectanglePro({GetScreenWidth()/2.0f, GetScreenHeight()/2.0f+100, 40, 40}, {20, 20},
+                (clock()/4) % 360, WHITE);
+        } else {
+            int textWidth = MeasureText("VICTORY :D", 35);
+            DrawText("VICTORY :D", (GetScreenWidth() / 2) - (textWidth / 2), 225, 35, YELLOW);
+        }
 
         EndDrawing();
     }
