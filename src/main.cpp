8bc5b58b29838abfd30a54b4d3e5b5db89ff8d10
PixelOyuncu <pixeloyuncuplayz@gmail.com>
2026-06-11 17:37:09 +0300

    bigo fixo

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,56 @@
 
 #include <iostream>
 #include <raylib.h>
+#include <algorithm>
+#include <cstring>
 #include <thread>
+
+#include <supertext.h>
+
+std::string initText = "Initializing";
+bool loaded = false;
+
+// handle loading string
+void loadingScreen() {
+    while (rand()%100<90) {
+        if (std::count(initText.begin(), initText.end(), '.') == 3) {
+            initText = "Initializing.";
+        } else {
+            initText += ".";
+        }
+        std::cout << initText << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+
+    loaded = true;
+}
 
 int main() {
     InitWindow(800, 450, "Touhou: Through the Bamboo Forest");
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+
+    std::thread loadingThread(loadingScreen); // thread for loading text
+
+    while (!loaded) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        int fontSize = 24;
+        int textWidth = MeasureText(initText.c_str(), fontSize);
+
+        DrawText(initText.c_str(), (GetScreenWidth() / 2) - (textWidth / 2), 225, fontSize, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        int textWidth = MeasureText("VICTORY :D", 35);
+        DrawText("VICTORY :D", (GetScreenWidth() / 2) - (textWidth / 2), 225, 35, LIGHTGRAY);
+
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
